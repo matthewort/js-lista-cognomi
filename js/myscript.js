7634422787d3f8b83430a2e7d90b63ead4dc54e0
Matteo Ferrarese <matteo.ferrarese@yahoo.it>
2020-10-23 17:10:29 +0200

    Struttura1

---
+++ js/myscript.js
@@ -0,0 +1,6 @@
+// chiedo il cognome
+var richiesta = prompt("Cognome?");
+// array cognomi
+var cognomi = ["Bernardi", "Galli", "Gentile", "Lombardi"];
+// lista ordinata alfabeticamente
+cognomi.push(richiesta);
