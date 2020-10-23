5246bbd55f7461c4932f7b6c703c572f8066108a
Matteo Ferrarese <matteo.ferrarese@yahoo.it>
2020-10-23 17:58:01 +0200

    Struttura2

--- js/myscript.js
+++ js/myscript.js
@@ -2,5 +2,17 @@
 var richiesta = prompt("Cognome?");
 // array cognomi
 var cognomi = ["Bernardi", "Galli", "Gentile", "Lombardi"];
+console.log(cognomi);
 // lista ordinata alfabeticamente
 cognomi.push(richiesta);
+
+
+var i = 0;
+while (i < cognomi.length && emailTrovata === false) {
+  console.log(i);
+  var compilazione = email[i];
+  // condizione mail corretta o errata
+  if (compilazione === richiesta) {
+    emailTrovata = true;
+  }
+  i++;
